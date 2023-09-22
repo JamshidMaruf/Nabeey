755e869638b3b912e31d54b7c2c7d305fcb6671c
misterpozitiv1607 <mansurjonmoydinov16072002@gmail.com>
2023-09-22 19:48:18 +0500

    DbSet added

--- src/Nabeey.DataAccess/Contexts/AppDbContext.cs
+++ src/Nabeey.DataAccess/Contexts/AppDbContext.cs
@@ -1,4 +1,13 @@
 using Microsoft.EntityFrameworkCore;
+using Nabeey.Domain.Entities.Answers;
+using Nabeey.Domain.Entities.Articles;
+using Nabeey.Domain.Entities.Assets;
+using Nabeey.Domain.Entities.Books;
+using Nabeey.Domain.Entities.Contexts;
+using Nabeey.Domain.Entities.QuestionAnswers;
+using Nabeey.Domain.Entities.Questions;
+using Nabeey.Domain.Entities.Quizzes;
+using Nabeey.Domain.Entities.Users;
 
 namespace Nabeey.DataAccess.Contexts;
 
@@ -6,4 +15,20 @@ public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     { }
+    public DbSet<Answer> Answers { get; set; }
+    public DbSet<Article> Articles { get; set; }
+    public DbSet<Asset> Assets { get; set; }
+    public DbSet<Book> Books { get; set; }
+    public DbSet<ContentBook> ContentBooks { get; set; }
+    public DbSet<Content> Contents { get; set; }
+    public DbSet<ContentAudio> ContentAudios { get; set; }
+    public DbSet<ContentCategory> ContentCategories { get; set; }
+    public DbSet<ContentImage> ContentImages { get; set; }
+    public DbSet<ContentVideo> ContentVideos { get; set; }
+    public DbSet<Question> Questions { get; set; }
+    public DbSet<QuestionAnswer> QuestionAnswers { get; set; }
+    public DbSet<Quiz> Quizzes { get; set; }
+    public DbSet<QuizQuestion> QuizQuestions { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserArticle> UserArticles { get; set; }
 }
