4f815a803f39ce15a664ce73452c59321cfb0a92
abdurrohmanq <qodirovabdurahmon46@gmail.com>
2023-09-22 19:35:51 +0500

    Quiz and QuizQuestion Added

---
+++ src/Nabeey.Service/DTOs/Articles/ArticleUpdateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Nabeey.Service.DTOs.Articles;
+
+public class ArticleUpdateDto
+{
+    public long Id { get; set; }
+    public string Text { get; set; }
+    public long ContentId { get; set; }
+}
