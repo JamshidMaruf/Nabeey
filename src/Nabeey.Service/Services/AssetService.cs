a053f3a6ab3468902f0ef101c82112afb85d4067
Nurullo <ghostone234qwer@gmail.com>
2023-09-22 19:54:26 +0500

    Write path

--- src/Nabeey.Service/Services/AssetService.cs
+++ src/Nabeey.Service/Services/AssetService.cs
@@ -1,6 +1,8 @@
 ﻿using Nabeey.DataAccess.IRepositories;
 using Nabeey.Domain.Entities.Assets;
 using Nabeey.Service.DTOs.Assets;
+using Nabeey.Service.Extensions;
+using Nabeey.Service.Helpers;
 using Nabeey.Service.Interfaces;
 
 namespace Nabeey.Service.Services;
@@ -14,13 +16,40 @@ public class AssetService : IAssetService
         this.repository = repository;
     }
 
-    public Task<bool> RemoveAsync(Asset asset)
+    public async Task<Asset> UploadAsync(AssetCreationDto dto)
     {
-        throw new NotImplementedException();
+        var webRootPath = Path.Combine(PathHelper.WebRootPath, "Images");
+
+        if (!Directory.Exists(webRootPath))
+            Directory.CreateDirectory(webRootPath);
+
+        var fileExtention = Path.GetExtension(dto.FormFile.FileName);
+        var fileName = $"{Guid.NewGuid().ToString("N")}{fileExtention}";
+        var filePath = Path.Combine(webRootPath, fileName);
+
+        var fileStream = new FileStream(filePath, FileMode.OpenOrCreate);
+        await fileStream.WriteAsync(dto.FormFile.ToByte());
+
+        var asset = new Asset()
+        {
+            FileName = fileName,
+            FilePath = filePath,
+        };
+
+
+        return asset;
     }
 
-    public Task<Asset> UploadAsync(AssetCreationDto dto)
+    public async Task<bool> RemoveAsync(Asset Assetment)
     {
-        throw new NotImplementedException();
+        if (Assetment is null)
+            return false;
+
+        var existAssetment = await repository.SelectAsync(a => a.Id.Equals(Assetment.Id));
+
+        if (existAssetment is null)
+            return false;
+
+        return true;
     }
 }
