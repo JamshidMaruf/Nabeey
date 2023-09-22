f83757bd0e97005d938f70b096ce7a0b54a1b4d2
Abboskhan3 <abbosnewz1@gmail.com>
2023-09-22 17:25:46 +0300

    update user Dto

--- src/Nabeey.Service/DTOs/Users/UserCreationDto.cs
+++ src/Nabeey.Service/DTOs/Users/UserCreationDto.cs
@@ -1,6 +1,21 @@
-﻿namespace Nabeey.Service.DTOs.Users;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Nabeey.Service.DTOs.Users;
 
 public class UserCreationDto
 {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
 
+    [Required, Phone]
+    public string Phone { get; set; }
+
+    [Required, EmailAddress]
+    public string Email { get; set; }
+
+    [Required, StringLength(100, MinimumLength = 6)]
+    public string Password { get; set; } = string.Empty;
+
+    [Compare("Password", ErrorMessage = "The passwords do not match.")]
+    public string ConfirmPassword { get; set; } = string.Empty;
 }
