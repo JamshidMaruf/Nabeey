f0fb7b005bdbc117b5849669350f5b80287e7a06
RavshanbekIsroilov <isroilovravshan42@gmail.com>
2023-09-22 19:02:03 +0500

    add collections

--- src/Nabeey.Domain/Questions/Question.cs
+++ src/Nabeey.Domain/Questions/Question.cs
@@ -1,11 +1,13 @@
 ﻿using Nabeey.Domain.Assets;
 using Nabeey.Domain.Commons;
+using Nabeey.Domain.QuestionAnswers;
 
-namespace Nabeey.Domain.Question;
+namespace Nabeey.Domain.Questions;
 
 public class Question:Auditable
 {
     public long AssetId { get; set; }
     public Asset Asset { get; set; }
     public string Text { get; set; }
+    public ICollection<QuestionAnswer> QuestionAnswers { get; set; }
 }
