d58cbe52549e90c65d36d3cd3aec3135c1159dc6
Saidkamolxon <saidkamolxon1997@gmail.com>
2023-09-22 19:29:06 +0500

    Tested Answer, Question, QuestionAnswer entities

--- src/Nabeey.Domain/Entities/Questions/Question.cs
+++ src/Nabeey.Domain/Entities/Questions/Question.cs
@@ -1,4 +1,4 @@
-﻿using Nabeey.Domain.Assets;
+﻿using Nabeey.Domain.Entities.Assets;
 using Nabeey.Domain.Commons;
 using Nabeey.Domain.Entities.QuestionAnswers;
 
@@ -6,8 +6,10 @@ namespace Nabeey.Domain.Entities.Questions;
 
 public class Question : Auditable
 {
+    public string Text { get; set; }
+
     public long AssetId { get; set; }
     public Asset Asset { get; set; }
-    public string Text { get; set; }
+
     public ICollection<QuestionAnswer> QuestionAnswers { get; set; }
 }
