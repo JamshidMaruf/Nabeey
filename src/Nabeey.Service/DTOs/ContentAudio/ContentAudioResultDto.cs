c5776e96f70d6a48cf6c689e3ff2bb50a86c5bbb
JSobirov <jasurbey2@gmail.com>
2023-09-22 19:16:07 +0500

    ContentVideo and ContentAudio DTOs added

---
+++ src/Nabeey.Service/DTOs/ContentAudio/ContentAudioResultDto.cs
@@ -0,0 +1,10 @@
+﻿using Nabeey.Service.DTOs.Assets;
+using Nabeey.Service.DTOs.Contents;
+
+namespace Nabeey.Service.DTOs.ContentAudio;
+
+public class ContentAudioResultDto
+{
+    public ContentResultDto Content { get; set; }
+    public AssetResultDto Asset { get; set; }
+}
