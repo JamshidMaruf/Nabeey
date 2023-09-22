b87614852250e1fb54192baa84e29581e1c83455
Ferganskiy <odilovmadiyor029@gmail.com>
2023-09-22 19:41:18 +0500

    Converter added

--- src/Nabeey.Service/Extensions/Converter.cs
+++ src/Nabeey.Service/Extensions/Converter.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Http;
 
-namespace Nabeey.Service.Extensions
+namespace Nabeey.Service.Extensions;
+
+public static class Converter
 {
-    internal class Converter
+    public static byte[] ToByte(this IFormFile formFile)
     {
+        using var memoryStream = new MemoryStream();
+        formFile.CopyTo(memoryStream);
+        return memoryStream.ToArray();
     }
 }
