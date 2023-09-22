222386a02854260bd21ca3ef3bcc9d090ea1777f
Avazbek0704 <siddikovavazbek213@gmail.com>
2023-09-22 19:32:52 +0500

    Book and ContentBook Dtos were created

---
+++ src/Nabeey.Service/DTOs/Books/BookUpdateDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Nabeey.Service.DTOs.Books;
+
+public class BookUpdateDto
+{
+    public long Id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Description { get; set; }
+    public long AssetId { get; set; }
+    public string Text { get; set; }
+}
