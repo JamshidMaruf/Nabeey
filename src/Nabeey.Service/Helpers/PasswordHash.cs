41a2492275ad4fc5ce9e71ac4d248279f44bc225
TMuhammadqodir <muhammadqodirturdiyev65@gmail.com>
2023-09-22 18:37:59 +0500

    Add passwordHash

---
+++ src/Nabeey.Service/Helpers/PasswordHash.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography;
+
+namespace Service.Helpers;
+
+public class PasswordHash
+{
+    public static void Encrypt(string password, out byte[] passwordhash, out byte[] passwordsalt)
+    {
+        using(var hmac=new HMACSHA512())
+        {
+            passwordsalt = hmac.Key;
+            passwordhash=hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+        }
+    }
+    public static bool VerifyPasswordHash(string password, byte[] hash, byte[] salt)
+    {
+        using (var hmac = new HMACSHA512(salt))
+        {
+            var computedHash =
+                hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            return computedHash.SequenceEqual(hash);
+        }
+    }
+}
