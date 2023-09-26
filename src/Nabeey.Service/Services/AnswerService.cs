8a2d625f6226ac21b0ddeb613c128805cdca97e4
Avazbek0704 <siddikovavazbek213@gmail.com>
2023-09-26 16:24:00 +0500

    UserService was fixed

--- src/Nabeey.Service/Services/AnswerService.cs
+++ src/Nabeey.Service/Services/AnswerService.cs
@@ -1,76 +1,85 @@
 ﻿using AutoMapper;
-using Microsoft.EntityFrameworkCore;
-using Nabeey.DataAccess.IRepositories;
-using Nabeey.Domain.Configurations;
-using Nabeey.Domain.Entities.Answers;
-using Nabeey.Service.DTOs.Answers;
 using Nabeey.Service.Exceptions;
 using Nabeey.Service.Interfaces;
+using Nabeey.Service.DTOs.Answers;
+using Nabeey.Domain.Configurations;
+using Microsoft.EntityFrameworkCore;
+using Nabeey.Domain.Entities.Answers;
+using Nabeey.DataAccess.IRepositories;
+using Nabeey.Domain.Entities.Questions;
+using Nabeey.Service.Extensions;
 
 namespace Nabeey.Service.Services;
 
 public class AnswerService : IAnswerService
 {
-    private readonly IRepository<Answer> repository;
     private readonly IMapper mapper;
-    public AnswerService(IRepository<Answer> repository, IMapper mapper)
+    private readonly IRepository<Answer> repository;
+    private readonly IRepository<Question> quetionRepository;
+
+    public AnswerService(IRepository<Answer> repository, IMapper mapper, IRepository<Question> quetionRepository)
     {
-        this.repository = repository;
         this.mapper = mapper;
+        this.repository = repository;
+        this.quetionRepository = quetionRepository;
     }
 
     public async ValueTask<AnswerResultDto> AddAsync(AnswerCreationDto dto)
     {
-        var mapAnswer = mapper.Map<Answer>(dto);
-        await repository.InsertAsync(mapAnswer);
-        await repository.SaveAsync();
+        Question existQuestion = await this.quetionRepository.SelectAsync(q => q.Equals(dto.QuestionId))
+            ?? throw new NotFoundException($"This quetionId is not found {dto.QuestionId}");
 
-        var res = mapper.Map<AnswerResultDto>(mapAnswer);
-        return res;
+        Answer mapAnswer = mapper.Map<Answer>(dto);
+        await this.repository.InsertAsync(mapAnswer);
+        await this.repository.SaveAsync();
+
+        return this.mapper.Map<AnswerResultDto>(mapAnswer);
     }
 
     public async ValueTask<AnswerResultDto> ModifyAsync(AnswerUpdateDto dto)
     {
-        var answer = await repository.SelectAsync(x => x.Id.Equals(dto.Id))
-            ?? throw new NotFoundException("Not found!");
+        Answer answer = await this.repository.SelectAsync(x => x.Id.Equals(dto.Id))
+            ?? throw new NotFoundException($"This AnswerId is not found {dto.Id}");
 
-        var mapAnswer = mapper.Map(dto, answer);
-        repository.Update(mapAnswer);
-        await repository.SaveAsync();
+        Answer mapAnswer = this.mapper.Map(dto, answer);
+        this.repository.Update(mapAnswer);
+        await this.repository.SaveAsync();
 
-        var res = mapper.Map<AnswerResultDto>(mapAnswer);
-        return res;
+        return this.mapper.Map<AnswerResultDto>(mapAnswer);
     }
 
     public async ValueTask<bool> RemoveAsync(long id)
     {
-        var answer = await repository.SelectAsync(x => x.Id.Equals(id))
-            ?? throw new NotFoundException("Not found!");
+        Answer existAnswer = await this.repository.SelectAsync(x => x.Id.Equals(id))
+            ?? throw new NotFoundException($"This id:{id} is not found ");
 
-        repository.Delete(answer);
-        await repository.SaveAsync();
+        this.repository.Delete(existAnswer);
+        await this.repository.SaveAsync();
 
         return true;
     }
 
     public async ValueTask<AnswerResultDto> RetrieveByIdAsync(long id)
     {
-        var answer = await repository.SelectAsync(x => x.Id.Equals(id))
-            ?? throw new NotFoundException($"Could not find id {id}");
+        Answer answer = await repository.SelectAsync(x => x.Id.Equals(id))
+            ?? throw new NotFoundException($"This id:{id} is not found ");
 
-        var res = mapper.Map<AnswerResultDto>(answer);
-        return res;
+        return this.mapper.Map<AnswerResultDto>(answer);
     }
 
     public async ValueTask<IEnumerable<AnswerResultDto>> RetrieveAllAsync(PaginationParams @params)
     {
-        var answer = await repository.SelectAll().ToListAsync();
-        var res = mapper.Map<IEnumerable<AnswerResultDto>>(answer);
-        return res;
+        var answers = (await this.repository.SelectAll().ToListAsync())
+                                            .ToPaginate(@params);
+
+        return this.mapper.Map<IEnumerable<AnswerResultDto>>(answers);
     }
 
-    public ValueTask<IEnumerable<AnswerResultDto>> RetrieveAllByQuestionIdAsync(long questionId)
+    public async ValueTask<IEnumerable<AnswerResultDto>> RetrieveAllByQuestionIdAsync(long questionId)
     {
-        throw new NotImplementedException();
+        var answers = await this.repository.SelectAll().Where(q => q.QuestionId == questionId).ToListAsync()
+            ?? throw new NotFoundException($"This quetionId:{questionId} is not found ");
+
+        return this.mapper.Map<IEnumerable<AnswerResultDto>>(answers);
     }
 }
