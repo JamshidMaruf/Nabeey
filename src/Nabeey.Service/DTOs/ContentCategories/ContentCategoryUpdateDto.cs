1a7b4e64b2b0fed9e3d026c7c2a6d76e330bc559
TMuhammadqodir <muhammadqodirturdiyev65@gmail.com>
2023-09-26 12:22:22 +0500

    Add ContentCategory service, controller and fixed QuizzService

---
+++ src/Nabeey.Service/DTOs/ContentCategories/ContentCategoryUpdateDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Nabeey.Service.DTOs.ContentCategories;
+
+public class ContentCategoryUpdateDto
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+}
---
+++ src/Nabeey.Service/Interfaces/IContentCategoryService.cs
@@ -0,0 +1,12 @@
+﻿using Nabeey.Service.DTOs.ContentCategories;
+
+namespace Nabeey.Service.Interfaces;
+
+public interface IContentCategoryService
+{
+    ValueTask<ContentCategoryResultDto> AddAsync(ContentCategoryCreationDto dto);
+    ValueTask<ContentCategoryResultDto> ModifyAsync(ContentCategoryUpdateDto dto);
+    ValueTask<bool> RemoveAsync(long id);
+    ValueTask<ContentCategoryResultDto> RetrieveByIdAsync(long id);
+    ValueTask<IEnumerable<ContentCategoryResultDto>> RetrieveAllAsync();
+}
