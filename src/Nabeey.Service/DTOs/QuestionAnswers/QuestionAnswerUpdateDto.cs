653d1f502a1ed64c66d343334c383b5cca633391
wonderboy1w3 <wonderboy1w3@gmail.com>
2023-09-26 14:31:51 +0300

    added changes

---
+++ src/Nabeey.Service/DTOs/QuestionAnswers/QuestionAnswerUpdateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Nabeey.Service.DTOs.QuestionAnswers;
+
+public class QuestionAnswerUpdateDto
+{
+    public long Id { get; set; }
+    public long AnswerId { get; set; }
+    public long QuestionId { get; set; }
+    public long QuizId { get; set; }
+}
---
+++ src/Nabeey.Service/DTOs/Quizzes/UserRatingDto.cs
@@ -0,0 +1,10 @@
+﻿using Nabeey.Service.DTOs.Users;
+
+namespace Nabeey.Domain.Entities.Quizzes;
+
+public class UserRatingDto
+{
+	public short Rating { get; set; }
+	public int Ball { get; set; }
+	public UserResultDto User { get; set; }
+}
