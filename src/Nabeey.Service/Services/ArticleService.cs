4c5c1d7770d6d9bbd47e4a31a9cfbeb89ed1fa77
abdurrohmanq <qodirovabdurahmon46@gmail.com>
2023-09-28 13:00:15 +0500

    ArticleService  bugs has been fixed

--- src/Nabeey.Service/Services/ArticleService.cs
+++ src/Nabeey.Service/Services/ArticleService.cs
@@ -42,16 +42,8 @@ public class ArticleService : IArticleService
 		var user = await this.userRepository.SelectAsync(u => u.Id.Equals(dto.UserId))
 			?? throw new NotFoundException("This user is not Found");
 
-		var imageAsset = await this.assetService.UploadAsync(new AssetCreationDto { FormFile = dto.Image }, UploadType.Images);
-
 		var existCategory = await this.categoryRepository.SelectAsync(a => a.Id.Equals(dto.CategoryId))
 			?? throw new NotFoundException($"This content is not found with id : {dto.CategoryId}");
-
-		var createImage = new Asset
-		{
-			FileName = imageAsset.FileName,
-			FilePath = imageAsset.FilePath,
-		};
 
 		var mapped = new Article
 		{
@@ -59,12 +51,23 @@ public class ArticleService : IArticleService
 			CategoryId = dto.CategoryId,
 			Category = existCategory,
 			UserId = dto.UserId,
-			User = user,
-			ImageId = imageAsset.Id,
-			Image = createImage,
+			User = user
 		};
 
-		await this.articleRepository.InsertAsync(mapped);
+        if (dto.Image is not null)
+        {
+            var uploadedImage = await this.assetService.UploadAsync(new AssetCreationDto { FormFile = dto.Image }, UploadType.Images);
+			var createImage = new Asset()
+			{
+				FileName = uploadedImage.FileName,
+				FilePath = uploadedImage.FilePath,
+			};
+
+            mapped.ImageId = uploadedImage.Id;
+            mapped.Image = createImage;
+        }
+
+        await this.articleRepository.InsertAsync(mapped);
 		await this.articleRepository.SaveAsync();
 
 		return this.mapper.Map<ArticleResultDto>(mapped);
@@ -72,7 +75,7 @@ public class ArticleService : IArticleService
 
 	public async ValueTask<bool> DeleteAsync(long id)
 	{
-		var existArticle = await this.articleRepository.SelectAsync(a => a.Id.Equals(id))
+		var existArticle = await this.articleRepository.SelectAsync(a => a.Id.Equals(id),includes: new[] {"Image"})
 		   ?? throw new NotFoundException($"This article is not found with id : {id}");
 
 		await this.assetService.RemoveAsync(existArticle.Image);
@@ -84,11 +87,33 @@ public class ArticleService : IArticleService
 
 	public async ValueTask<ArticleResultDto> ModifyAsync(ArticleUpdateDto dto)
 	{
-		var existArticle = await this.articleRepository.SelectAsync(a => a.Id.Equals(dto.Id))
+		var existArticle = await this.articleRepository.SelectAsync(a => a.Id.Equals(dto.Id), includes: new[] { "Image" })
 			?? throw new NotFoundException($"This article is not found with id : {dto.Id}");
 
-		this.mapper.Map(dto, existArticle);
-		this.articleRepository.Update(existArticle);
+        var existCategory = await this.categoryRepository.SelectAsync(a => a.Id.Equals(dto.CategoryId))
+            ?? throw new NotFoundException($"This content is not found with id : {dto.CategoryId}");
+
+        var uploadedImage = new Asset();
+        if (dto.Image is not null)
+        {
+            uploadedImage = await this.assetService.UploadAsync(new AssetCreationDto { FormFile = dto.Image }, UploadType.Images);
+            await this.assetService.RemoveAsync(existArticle.Image);
+        }
+		existArticle.Text = dto.Text;
+		existArticle.CategoryId = dto.CategoryId;
+
+		if(uploadedImage.Id > 0)
+		{
+            if (existArticle.Image == null)
+            {
+                existArticle.Image = new Asset();
+            }
+            existArticle.ImageId = uploadedImage.Id;
+			existArticle.Image.FileName = uploadedImage.FileName;
+			existArticle.Image.FilePath = uploadedImage.FilePath;
+		}
+
+        this.articleRepository.Update(existArticle);
 		await this.articleRepository.SaveAsync();
 
 		return this.mapper.Map<ArticleResultDto>(existArticle);
@@ -97,7 +122,7 @@ public class ArticleService : IArticleService
 
 	public async ValueTask<ArticleResultDto> RetrieveAsync(long id)
 	{
-		var existArticle = await this.articleRepository.SelectAsync(expression: u => u.Id == id, includes: new[] { "Image" })
+		var existArticle = await this.articleRepository.SelectAsync(expression: u => u.Id == id, includes: new[] { "User", "Category", "Image" })
 		   ?? throw new NotFoundException($"This article is not found with id : {id}");
 
 		return this.mapper.Map<ArticleResultDto>(existArticle);
@@ -105,10 +130,10 @@ public class ArticleService : IArticleService
 
 	public async ValueTask<IEnumerable<ArticleResultDto>> RetrieveAllByUserIdAsync(long userId)
 	{
-		var existUser = await this.userRepository.SelectAsync(expression: u => u.Id == userId, includes: new[] { "Image" })
+		var existUser = await this.userRepository.SelectAsync(expression: u => u.Id == userId, includes: new[] {"Asset" })
 			?? throw new NotFoundException("This user is not found");
 
-		var userArticles = await this.articleRepository.SelectAll(a => a.UserId == userId).ToListAsync();
+		var userArticles = await this.articleRepository.SelectAll(a => a.UserId == userId, includes: new[] { "User","Category", "Image" }).ToListAsync();
 
 		return this.mapper.Map<IEnumerable<ArticleResultDto>>(userArticles);
 	}
@@ -118,14 +143,14 @@ public class ArticleService : IArticleService
 		var existCategory = await this.categoryRepository.SelectAsync(expression: u => u.Id == categoryId, includes: new[] { "Image" })
 			?? throw new NotFoundException("This user is not found");
 
-		var contentArticles = await this.articleRepository.SelectAll(a => a.UserId == categoryId).ToListAsync();
+		var contentArticles = await this.articleRepository.SelectAll(a => a.CategoryId == categoryId ,includes: new[] { "User", "Category", "Image" }).ToListAsync();
 
 		return this.mapper.Map<IEnumerable<ArticleResultDto>>(contentArticles);
 	}
 
 	public async ValueTask<IEnumerable<ArticleResultDto>> RetrieveAllAsync(PaginationParams @params, Filter filter, string search = null)
 	{
-		var allArticles = (await this.articleRepository.SelectAll(includes: new[] { "Image" })
+		var allArticles = (await this.articleRepository.SelectAll(includes: new[] {"User.Asset","Category", "Image" })
 			.ToPaginate(@params)
 			.ToListAsync());
 
