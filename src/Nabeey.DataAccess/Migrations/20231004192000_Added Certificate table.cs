3182bd545346e55c1cc17473e74f32c4c7ed7e9a
wonderboy1w3 <wonderboy1w3@gmail.com>
2023-10-04 22:26:32 +0300

    Added changes

---
+++ src/Nabeey.DataAccess/Migrations/20231004192000_Added Certificate table.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Nabeey.DataAccess.Migrations
+{
+    public partial class AddedCertificatetable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
